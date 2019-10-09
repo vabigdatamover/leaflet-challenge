c7b3559769ccd68b5c0abf721e1b7b0e2a37eed2
vabigdatamover <vabigdatamover@gmail.com>
2019-10-08 20:30:44 -0400

    Leaflet Update

--- logic.js
+++ logic.js
@@ -1,14 +1,14 @@
 // Creating map object
 var map = L.map("map", {
-    center: [40.7128, -74.0059],
-    zoom: 1
+    center: [38.89511, -77.03637],
+    zoom: 6
   });
   
   // Adding tile layer
   // L.tileLayer("https://api.mapbox.com/v4/mapbox.satellite/1/0/0@2x.jpg90?access_token={accessToken}",{
   L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-    maxZoom: 1,
+    maxZoom: 11,
     id: "mapbox.streets",
     accessToken: API_KEY
   }).addTo(map);
