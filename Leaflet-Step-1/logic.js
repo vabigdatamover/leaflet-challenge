f13fa4da8e00fa11e681f88bb38bb6df0477c6ad
vabigdatamover <vabigdatamover@gmail.com>
2019-10-10 13:18:43 -0400

    Leaflet Update

--- Leaflet-Step-1/logic.js
+++ Leaflet-Step-1/logic.js
@@ -4,9 +4,10 @@ var myMap = L.map("map", {
   zoom: 6
 });
 
-// Adding tile layer
-L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+//Gray Map Option
+//var graymap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href='https://www.openstreetmap.org/'>OpenStreetMap</a> contributors, <a href='https://creativecommons.org/licenses/by-sa/2.0/'>CC-BY-SA</a>, Imagery © <a href='https://www.mapbox.com/'>Mapbox</a>",
   maxZoom: 18,
   id: "mapbox.streets",
   accessToken: API_KEY
@@ -15,64 +16,100 @@ L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
 // Load in geojson data
 var geoData = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
-var geojson;
+// var geojson;
 
 //Grab data with d3
-d3.json(geoData, function(data) {
+d3.json(geoData, function (data) {
 
-  // Create a new choropleth layer
-  geojson = L.choropleth(data, {
+  // This function returns the style data for each of the earthquakes we plot on
+  // the map. We pass the magnitude of the earthquake into two separate functions
+  // to calculate the color and radius.
+  function styleInfo(feature) {
+    return {
+      opacity: 1,
+      fillOpacity: 1,
+      fillColor: getColor(feature.properties.mag),
+      color: "#000000",
+      radius: getRadius(feature.properties.mag),
+      stroke: true,
+      weight: 0.5
+    };
+  }
 
-    // Define what  property in the features to use
-    // valueProperty: "MHI2016",
+  // This function determines the color of the marker based on the magnitude of the earthquake.
+  function getColor(magnitude) {
+    switch (true) {
+    case magnitude > 5:
+      return "#ea2c2c";
+    case magnitude > 4:
+      return "#ea822c";
+    case magnitude > 3:
+      return "#ee9c00";
+    case magnitude > 2:
+      return "#eecc00";
+    case magnitude > 1:
+      return "#d4ee00";
+    default:
+      return "#98ee00";
+    }
+  }
 
-    // Set color scale
-    scale: ["#3e7f22", "#7f4f22"],
+  // This function determines the radius of the earthquake marker based on its magnitude.
+  // Earthquakes with a magnitude of 0 were being plotted with the wrong radius.
+  function getRadius(magnitude) {
+    if (magnitude === 0) {
+      return 1;
+    }
 
-    // Number of breaks in step range
-    steps: 10,
+    return magnitude * 4;
+  }
 
-    // q for quartile, e for equidistant, k for k-means
-    mode: "q",
-    style: {
-      // Border color
-      color: "#fff",
-      weight: 1,
-      fillOpacity: 0.8
+
+  L.geoJson(data, {
+
+    pointToLayer: function (feature, latlong){
+      return L.circleMarker(latlong);
     },
 
+    style: styleInfo,
+
+    
+
     // Binding a pop-up to each layer
-    onEachFeature: function(feature, layer) {
+    onEachFeature: function (feature, layer) {
+
       layer.bindPopup("Earthquake Magnitude: " + feature.properties.mag + "<br>Earthquake Location:<br>" + feature.properties.place);
     }
   }).addTo(myMap);
 
-  // Set up the legend
-  var legend = L.control({ position: "bottomright" });
-  legend.onAdd = function() {
-    var div = L.DomUtil.create("div", "info legend");
-    var limits = geojson.options.limits;
-    var colors = geojson.options.colors;
-    var labels = [];
+  // // Set up the legend
+  // var legend = L.control({ position: "bottomright" });
+  // legend.onAdd = function() {
+  //   var div = L.DomUtil.create("div", "info legend");
+  //   // var limits = geojson.options.limits;
+  //   // var colors = geojson.options.colors;
+  //   // var labels = [];
 
-    // Add min & max
-    var legendInfo = "<h1>Earthquake Scale</h1>" +
-      "<div class=\"labels\">" +
-        "<div class=\"min\">" + limits[0] + "</div>" +
-        "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
-      "</div>";
+  //   // Add min & max
+  //   var legendInfo = "<h1>Earthquake Scale</h1>" +
+  //     "<div class=\"labels\">" +
+  //       // "<div class=\"min\">" + limits[0] + "</div>" +
+  //       // "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
+  //     "</div>";
 
-    div.innerHTML = legendInfo;
+  //   div.innerHTML = legendInfo;
 
-    limits.forEach(function(limit, index) {
-      labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
-    });
+    // limits.forEach(function(limit, index) {
+    //   labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
+    // });
 
-    div.innerHTML += "<ul>" + labels.join("") + "</ul>";
-    return div;
-  };
+  //   div.innerHTML += "<ul>" + labels.join("") + "</ul>";
+  //   return div;
+  // };
 
-  // Adding legend to the map
-  legend.addTo(myMap);
+  // // Adding legend to the map
+  // legend.addTo(myMap);
 
 });
+
+
