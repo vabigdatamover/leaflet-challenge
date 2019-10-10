4a6f2413c72e660b15bcb63f0179809bdeee0073
vabigdatamover <vabigdatamover@gmail.com>
2019-10-10 09:45:46 -0400

    Leaflet Update

--- logic.js
+++ logic.js
@@ -50,7 +50,7 @@ L.control.layers(null, overlays).addTo(map);
 L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token={accessToken}", {
  attribution: "Map data &copy; <a href='https://www.openstreetmap.org/'>OpenStreetMap</a> contributors, <a href='https://creativecommons.org/licenses/by-sa/2.0/'>CC-BY-SA</a>, Imagery © <a href='https://www.mapbox.com/'>Mapbox</a>",
  maxZoom: 18,
- id: "mapbox.light",
+ id: "mapbox.streets",
  accessToken: API_KEY
 }).addTo(myMap);
 
